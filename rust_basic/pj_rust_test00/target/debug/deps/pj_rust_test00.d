d6025020c9f284535e81a8701680696b6c96c11a
calixlock <calixlock@naver.com>
2024-03-02 23:32:13 +0900

    rust first project

---
+++ rust_basic/pj_rust_test00/target/debug/deps/pj_rust_test00.d
@@ -0,0 +1,5 @@
+E:\Bigdata\git\git_2023\calixlock\rust\rust_basic\pj_rust_test00\target\debug\deps\pj_rust_test00.exe: src\main.rs
+
+E:\Bigdata\git\git_2023\calixlock\rust\rust_basic\pj_rust_test00\target\debug\deps\pj_rust_test00.d: src\main.rs
+
+src\main.rs:
---
+++ rust_basic/pj_rust_test00/target/debug/pj_rust_test00.d
@@ -0,0 +1,1 @@
+E:\Bigdata\git\git_2023\calixlock\rust\rust_basic\pj_rust_test00\target\debug\pj_rust_test00.exe: E:\Bigdata\git\git_2023\calixlock\rust\rust_basic\pj_rust_test00\src\main.rs
